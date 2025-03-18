8f6f96538eb27f525968936624a0dee87283ea8c
Sachin Yadav <sachin.yadav@bcah.christuniversity.in>
2025-03-18 10:20:19 +0530

    Add project files.

---
+++ sqlexam/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace sqlexam
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Server=(localdb)\MSSQLLocalDB;Database=master;Integrated Security=True;";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string username = textBox1.Text.Trim();
+            string password = textBox2.Text.Trim();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "INSERT INTO testusers (name, pass) VALUES (@username, @password)";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@username", username);
+                    cmd.Parameters.AddWithValue("@password", password);
+                    int result = cmd.ExecuteNonQuery();
+
+                    if (result == 1)
+                    {
+                        MessageBox.Show("User added successfully");
+                    }
+                    else
+                    {
+                        MessageBox.Show("User not added");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+    }
+}
